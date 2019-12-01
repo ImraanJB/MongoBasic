49f12ac1f17a0b26703037b042b343784c5865ea
Imraan JB <imranjhabua@gmail.com>
2019-12-01 16:10:52 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,54 @@
+const mongoose = require('mongoose');
+
+mongoose.connect('mongodb://localhost/playground')
+    .then(() => console.log('Connected to DB...'))
+    .catch(err => console.error('Could not connect to MongoDB..', err));
+
+const courseSchema = new mongoose.Schema({
+    name: String,
+    author: String,
+    tags: [String],
+    date: { type:Date , default: Date.now },
+    isPublished : Boolean
+});
+
+const Course = mongoose.model('Course' , courseSchema );
+
+async function createCourse() {
+    //To create models in the db
+const course = new Course({
+    name : 'Angular Course',
+    author : 'MOSH',
+    tags: ['Angular', 'frontend'],
+    isPublished : true
+});
+
+const result = await course.save();
+console.log(result);
+}
+
+// createCourse();
+
+async function getCourses() {
+    //To Get models from DB
+    // Diiferent Comparison query operators of Mongo/Mongoose
+    // eq (equal)
+    // ne (not equal)
+    // gt (greater than)
+    // gte (greater than or equal to)
+    // lt (less than)
+    // lte (less than or equal to)
+    // in
+    // nin (not in) 
+
+    const courses = await Course
+    //find({ author:'MOSH', isPublished:true })
+    //.find({ price :{ $gte: 10 , $lte:20 }})
+    .find({ price :{ $in: [10,15,20] }})
+    .limit(10)
+    .sort({ name:1 })
+    .select({ name:1, tags:1 });
+    console.log(courses);
+}
+
+getCourses();
